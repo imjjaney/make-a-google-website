211dda46c8f9b250f481316395f35fea037c10d2
imjjaney <hmyeonge@naver.com>
2024-05-30 13:49:20 +0900

    lucky 버튼 클릭시 배경색 변경 기능 구현 및 툴킷 제거

---
+++ script.js
@@ -0,0 +1,14 @@
+var bodycolor = document.querySelector("body");
+var button = document.querySelector(".btn");
+
+var color = ["red", "orange", "pink", "blue", "green", "purple"]
+
+button.addEventListener("click", changeColor)
+
+function changeColor() {
+  var num = Math.floor(Math.random() * color.length);
+  var colorSelected = color[num];
+  console.log(colorSelected);
+
+  bodycolor.style.backgroundColor = colorSelected;
+}
